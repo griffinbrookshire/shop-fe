8bf5873def074fad01f7927d2e2fd06ead082560
Griffin Brookshire <gbrookshire5@gmail.com>
2023-11-01 21:26:28 -0400

    complete cart & add responsive styles

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,87 @@
+import { useEffect, useState } from "react"
+
 export default function Cart() {
 
+  const totalPrice = 49.99
+  const shippingPrice = 8.99
+  const taxPrice = 4.99
+  const [orderTotal, setOrderTotal] = useState(Math.floor((totalPrice + shippingPrice + taxPrice) * 100) / 100)
+
   function CartItem() {
-    
+
+    const itemName = "Super Sick T-Shirt"
+    const itemPrice = 19.99
+    const quantity = 1
+    const [itemTotal, setItemTotal] = useState(itemPrice*quantity)
+
+    function calcPrice(event) {
+      let quantity = event.target.value
+      setItemTotal(Math.floor(itemPrice*quantity * 100) / 100)
+      setOrderTotal(Math.floor((totalPrice + shippingPrice + taxPrice) * 100) / 100)
+    }
+
+    return (
+      <div className="flex py-8">
+        <img src="./shirt.png" className="w-[100px] h-[140px] mr-4 bg-gray-100 border border-gray-300 rounded-md"></img>
+        <div className="flex flex-col h-full text-left">
+          <p>{itemName}</p>
+          <div className="text-gray-400 mt-auto">
+            <p>Size: L</p>
+            <p>Color: Black</p>
+            <p>Material: Cotton</p>
+          </div>
+        </div>
+        <div className="flex flex-col h-full ml-auto items-end justify-between">
+          <div className="h-8">
+            <img src="trash.png" className="w-[16px] h-[16px]"></img>
+          </div>
+          <div className="h-8">
+            <input type="number" defaultValue={quantity} min={1} className="w-12 h-8 pl-2 pr-1 border-gray-300 border rounded-lg" onChange={calcPrice}></input>
+          </div>
+          <div className="flex items-end h-8 space-x-2">
+              <span>Total:</span><span>${itemTotal}</span>
+          </div>
+        </div>
+      </div>
+    )
+
+  }
+
+  function SummaryLineItem({title, value}) {
+
+    return (
+      <div className="flex justify-between py-1">
+        <span>{title}:</span>
+        <span>${value}</span>
+      </div>
+    )
+
   }
 
   return (
-    <div className="w-full mx-auto">
-        <p>This is your cart! Nice.</p>
+    <div className="w-full flex flex-col min-[1000px]:flex-row max-[1000px]:items-center min-[1000px]:justify-center mb-8 space-x-0 min-[1000px]:space-x-8 space-y-8 min-[1000px]:space-y-0">
+      <div className="w-1/2 min-w-[350px] max-w-[750px] text-center card">
+        <h1 className="text-2xl mb-2">My Cart</h1>
+        <div className="grid grid-cols-1 divide-y">
+          <CartItem></CartItem>
+          <CartItem></CartItem>
+          <CartItem></CartItem>
+          <CartItem></CartItem>
+        </div>
+      </div>
+      <div className="w-1/3 min-w-[350px] max-w-[750px] h-fit flex flex-col text-center card">
+        <h1 className="text-2xl mb-2">Summary</h1>
+        <div className="px-24">
+          <SummaryLineItem title="Subtotal" value={totalPrice}></SummaryLineItem>
+          <SummaryLineItem title="Shipping" value={shippingPrice}></SummaryLineItem>
+          <SummaryLineItem title="Tax" value={taxPrice}></SummaryLineItem>
+          <div className="flex justify-between py-2 font-bold">
+            <span>Order Total:</span>
+            <span>${orderTotal}</span>
+          </div>
+        </div>
+        <button className="w-40 h-10 mt-8 mx-auto bg-blue-400 text-lg text-white rounded-xl">Checkout</button>
+      </div>
     </div>
   )
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar() {
     return (
       <div className="flex items-center">
         <img src="gear.png" alt="settings" className="w-12 h-12"></img>
-        <span className="ml-2">Griff & Kal LLC</span>
+        <span className="ml-2 hidden min-[825px]:inline">Griff & Kal LLC</span>
       </div>
       
     )
@@ -22,20 +22,21 @@ export default function NavBar() {
     return (
       <form className="flex items-center">
         <label>Search</label>
-        <input type="text" className="ml-2 rounded-full b-2 border-gray-500 text-gray-700 px-2 hover:bg-gray-100"></input>
+        <input type="text" className="ml-2 px-4 rounded-full border-gray-500 border text-gray-700 hover:bg-gray-100"></input>
       </form>
     )
   }
 
   return (
-    <nav className="flex justify-between fixed top-0 left-0 w-screen m-0 px-12 py-6 bg-blue-400 shadow-lg text-white text-lg">
+    <nav className="flex justify-between fixed top-0 left-0 z-10 w-screen m-0 px-12 py-6 bg-blue-400 shadow-lg text-white text-lg">
       <Logo></Logo>
       <SearchBar></SearchBar>
-      <ul className="flex justify-center items-center space-x-8">
+      <ul className="flex flex-col min-[700px]:flex-row justify-center items-center space-x-0 min-[700px]:space-x-8">
         <NavItem text={'Home'} link="/"></NavItem>
         <NavItem text={'Orders'} link="/"></NavItem>
         <NavItem text={'Cart'} link="/cart"></NavItem>
       </ul>
     </nav>
   )
+
 }
